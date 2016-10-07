ef562185cefc6f8d9b3c706cd6c373aa8bf8249e
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-10-07 01:32:58 +0000

    last commit was trying to get result to display vertical, it did not work. This is the original working code.

--- countLetters.js
+++ countLetters.js
@@ -1,26 +1,13 @@
-// // write a function that returns all the unique characters that exist in a string
-// // and reports how many instances each letter is found in the string
-// //returns an object that can represent the stats for the sentence it is given: all characters found in
-// //input string.
-//hint: perhaps return an object where each unique character is a property of an
-// object. and the value for that
-//property/key should be the number of occurences for that character.
 var lighthouse = process.argv[2];
 var squish = lighthouse.replace(/ /g,'');
-//console.log(lighthouse);
-//console.log(squish);
-
 function countLetters(theStringPassedIn) {
   var counter = {
-    'n':'n'
   };
-  for (var i = 0; i < theStringPassedIn.length; i++){ // Do a clever thing with a loop
+  for (var i = 0; i < theStringPassedIn.length; i++){
     counter[theStringPassedIn[i]] = 0;
-   //console.log(theStringPassedIn[1])
-  }
+     }
   for (var letter of theStringPassedIn){
     counter[letter]+= 1;
-    //console.log(counter[letter]);
   }
   return counter;
 }
@@ -28,6 +15,3 @@ function countLetters(theStringPassedIn) {
 console.log(countLetters(squish));
 
 
-
-//The point is to get you to think about the problem and strip it to it's bare essentials
-//For example in this, they give you a string and you give them back an object.
